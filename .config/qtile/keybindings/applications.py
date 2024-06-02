de9470af65c1ca73ca67d3f7f35516736b90cb81
Brannigan Sakwah <brannigansakwah@gmail.com>
2024-06-02 10:03:44 +0300

    qtile screenshot shortcut

--- .config/qtile/keybindings/applications.py
+++ .config/qtile/keybindings/applications.py
@@ -1,7 +1,11 @@
 from libqtile.config import Key
 from libqtile.lazy import lazy
 
+import os
+
 def get_keybindings(mod: str, terminal: str) -> list[Key]:
+    home_dir = os.path.expanduser("~")
+    
     return [
         Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
         Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
@@ -11,4 +15,6 @@ def get_keybindings(mod: str, terminal: str) -> list[Key]:
         
         Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
         Key([mod], "space", lazy.spawn("ulauncher"), desc="Open Application Launcher"),
+        
+        Key([], "Print", lazy.spawn(f"scrot -s '{home_dir}/Pictures/Screenshots/Screenshot %Y-%m-%d %H_%M_%S.png'"), desc="Take a screenshot"),
     ]
